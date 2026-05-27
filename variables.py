d22a9646354cf3f07054486d5bf1d8544389b6fa
Saybal <roysaybal@gmail.com>
2026-05-27 22:15:35 +0600

    first commit

---
+++ variables.py
@@ -0,0 +1,42 @@
+# char_name = 'John'
+# char_age = '35'
+
+# print('There once a man named ' + char_name + ',')
+# print('he was ' + char_age + ' old. ')
+# print('He really like the name ' + char_name + ',')
+# print('but did not like being ' + char_age + '.')
+
+# # !Working with Strings 
+# print("I do not wanna do it.")
+# print("I do not wanna ... \n do it.")
+# print("I don\'t wanna do it.") # To print ' or " symbol use \
+
+# name = 'SAYBAL ROY' 
+# print(name.lower())
+# print(name.isupper())
+# print(name.islower())
+# print(name.lower().islower())
+# print(len(name))
+# print(name[5])
+# print(name.index('R'))
+# print(name.index('ROY')) #Used to find substring
+# # print(name.index('ZZZ')) #Throws an error with: Substring not found.
+# print(name.replace('ROY', 'RAY'))
+
+#!Working with numbers
+
+# reg_no = 2022331541
+# print("my registration number is: " + str(reg_no)) #Without conversion it will through a type error.
+# tempature = -23
+# print(abs(tempature))
+# print(pow(2,3)) #2^3 = 8
+# print(max(2,3)) #3
+# print(min(2,3)) #2
+# print(round(5.12233)) #5 --> if decimal part < 0.5
+# print(round(5.676)) #6 --> if decimal part > 0.5
+
+from math import *
+print(floor(3.9)) # return 3
+print(ceil(3.1)) # return 4
+print(sqrt(36)) # return square root in decimal form
+
