4b7e9360bc9817b44f920281421991877643f46d
Saybal <roysaybal@gmail.com>
2026-05-28 10:37:49 +0600

    Learned basic list

---
+++ lists.py
@@ -0,0 +1,32 @@
+odd = [1,3,5,7,9]
+random = [1, 'Arcane' , False, 'Stingy']
+
+# print(odd)
+# print(odd[3])
+# print(odd[3] - odd[1])
+# print(random)
+# print(random[2])
+# print(random[-2]) #In reverse or last of the array index starts from -1.
+# print(odd[1:]) #Grave from index 1 to end
+# print(odd[1:4]) #Grave from index 1 to index 3 (range)
+# random[0] = 'Render' #Changing an element
+# print(random)
+
+#!List Functions
+# random.extend(odd)
+# random.append(3) # Add a value at the end of the list.
+# random.insert(2, True) # Add value at random position
+# odd.remove(7)
+# random.clear() #Delete the entire list.
+# odd.pop() #Pop an element
+# print(odd.index(7))
+# print(odd.count(7)) #count frequency of an element
+# odd.sort()
+# print(odd)
+# odd.reverse()
+# print(odd)
+# print(random) 
+# print(odd)
+
+odd_2 = odd.copy()
+print(odd_2)
