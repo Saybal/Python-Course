535efa9fec1f476d2425fa976bdbc0cf20668612
Saybal <roysaybal@gmail.com>
2026-05-28 11:18:38 +0600

    Learned basic function with return

---
+++ Tuples.py
@@ -0,0 +1,3 @@
+odd = (1,3,5,7,9) #Tuple is immutable
+print(odd)
+print(odd[2])
---
+++ functions.py
@@ -0,0 +1,14 @@
+# def addition(num1, num2):
+#     print("Summation: ", num1 + num2)
+
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# addition(num1, num2)
+
+#!Return Staement
+def multiple(num1, num2):
+    return num1 * num2
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+result = multiple(num1, num2)
+print('Multiple: ', result)
