f858a777722667d42ac2bfad48cb40a630c114e0
Saybal <roysaybal@gmail.com>
2026-05-28 21:15:07 +0600

    learn basic class object

---
+++ student_class.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, name, Id, department, CGPA, isEnrolled):
+        self.name = name
+        self.Id = Id
+        self.department = department
+        self.CGPA = CGPA
+        self.isEnrolled = isEnrolled
---
+++ student_object.py
@@ -0,0 +1,5 @@
+from student_class import Student
+
+student_1 = Student('Saybal Roy', 2022331541, 'CSE', 3.64, True)
+student_2 = Student('Joy Dip Das', 2022331528, 'CSE', 3.58, True)
+print(student_2.name)
